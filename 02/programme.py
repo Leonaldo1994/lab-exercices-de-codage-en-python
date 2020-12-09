b1ccaadb3f13a57d359c585530b1aa50bb64e309
Leonaldo Decimus <leonaldodecimus@Leonaldos-MacBook-Pro.local>
2020-12-09 12:35:32 -0500

    ajout de la fonction exo_02() imprimant Bonjour

--- 02/programme.py
+++ 02/programme.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 """
 
-@author: CollegeBoreal
+@author: Leonaldo1994
 """
 
-def ???:
-  return ???
-
+def exo_02():
+  return ("Bonjour")
+   
 def main():
   print( exo_02() )
 
 if __name__== "__main__":
   main()
+
